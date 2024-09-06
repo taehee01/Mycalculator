03ec94a731f78b6f282a7a1d674108e57d5801db
taehee01 <taehui120115@gmail.com>
2024-09-06 19:37:26 +0900

    0906

--- app/src/main/java/com/example/mycalculator/GeomertyOptionActivity.java
+++ app/src/main/java/com/example/mycalculator/GeomertyOptionActivity.java
@@ -1,6 +1,8 @@
 package com.example.mycalculator;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,23 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class GeomertyOptionActivity extends AppCompatActivity {
+    Button btnCircle;
+    Button btnTrapezoid;
+    Button btnParallelogram;
+    Button btnTriangle;
+    Button btnRhombus;
+    ImageView imgMollu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_geomerty_option);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        btnCircle = findViewById(R.id.btnCircle);
+        btnParallelogram = findViewById(R.id.btnParallelogram);
+        btnTrapezoid = findViewById(R.id.btnTrapezoid);
+        btnTriangle = findViewById(R.id.btnTriangle);
+        btnRhombus = findViewById(R.id.btnRhombus);
+        imgMollu = findViewById(R.id.imageView2);
     }
 }
