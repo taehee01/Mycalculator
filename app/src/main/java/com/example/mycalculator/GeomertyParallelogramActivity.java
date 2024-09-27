640fa9edfa2ca96aeded659014906856494b0a4b
taehee01 <taehui120115@gmail.com>
2024-09-27 19:38:47 +0900

    0927

--- app/src/main/java/com/example/mycalculator/GeomertyParallelogramActivity.java
+++ app/src/main/java/com/example/mycalculator/GeomertyParallelogramActivity.java
@@ -1,6 +1,10 @@
 package com.example.mycalculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,27 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class GeomertyParallelogramActivity extends AppCompatActivity {
-
+    EditText editParallelogramA;
+    EditText editParallelogramB;
+    TextView textParallelogramResult;
+    Button btnParallelogramResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_geomerty_parallelogram);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        editParallelogramA = findViewById(R.id.editParallelogramA);
+        editParallelogramB = findViewById(R.id.editParallelogramB);
+        textParallelogramResult = findViewById(R.id.textParallelogramResult);
+        btnParallelogramResult = findViewById(R.id.btnParallelogramResult);
+        btnParallelogramResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double A= Double.parseDouble(editParallelogramA.getText().toString());
+                double B= Double.parseDouble(editParallelogramB.getText().toString());
+                double result = A*B/2;
+                textParallelogramResult.setText(""+result);
+            }
         });
     }
 }
